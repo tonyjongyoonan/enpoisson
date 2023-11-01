75ae3deed1b20f7712c6a4cebd547cb24fbc982a
nlao1 <nlao@seas.upenn.edu>
2023-11-01 16:22:05 -0400

    Login page

---
+++ frontend/src/Login.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./style.css";
+
+export const Box = () => {
+  return (
+    <div className="box">
+        // TODO: change me
+      <div className="maxshirko-me">
+        <div className="element">
+          <div className="overlap">
+            <div className="text-wrapper">Log In</div>
+            <div className="group">
+              <div className="overlap-group">
+                <div className="rectangle" />
+                <div className="div">password</div>
+              </div>
+            </div>
+            <p className="whatever-description">
+              whatever description we want again
+              <br />
+              en poission
+              <br />
+              built by team etc etc
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
