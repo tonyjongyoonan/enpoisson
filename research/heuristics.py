be353e5df1a421f6b29212908d2a24379e7bfeef
ishlal <ilal@seas.upenn.edu>
2023-11-21 18:13:23 -0500

    pawn + knight control

--- research/heuristics.py
+++ research/heuristics.py
@@ -2,45 +2,66 @@ import chess.pgn
 from stockfish import Stockfish
 import chess
 
+def get_column(square):
+    return square % 8
+
+def get_column_a(i, j):
+    return j
+
+def get_row(square):
+    return square // 8
+
+def get_row_a(i, j):
+    return i
+
 def is_passed_pawn_black(board, row, column): 
     for i in range(row-1, -1, -1):
-        if (board.piece_at(i*8+(column-1)) != None):
-            if (board.piece_at(i*8+(column-1)).symbol() == "P"):
+        if (board.piece_at(i*8 + column) != None):
+            if (board.piece_at(i*8 + column).symbol() == "P"):
                 return False
     for i in range(row-1, -1, -1):
-        if (board.piece_at(i*8+(column+1)) != None):
-            if (board.piece_at(i*8+(column+1)).symbol() == "P"):
-                return False
+        if (column >= 1):
+            if (board.piece_at(i*8+(column-1)) != None):
+                if (board.piece_at(i*8+(column-1)).symbol() == "P"):
+                    return False
+    for i in range(row-1, -1, -1):
+        if (column <= 6):
+            if (board.piece_at(i*8+(column+1)) != None):
+                if (board.piece_at(i*8+(column+1)).symbol() == "P"):
+                    return False
     return True
 
 def is_passed_pawn_white(board, row, column):
     # returns True if SQUARE is a passed pawn for COLOR
     for i in range(row+1, 8):
-        if (board.piece_at(i*8+(column-1)) != None):
-            if (board.piece_at(i*8+(column-1)).symbol() == "p"):
+        if (board.piece_at(i*8+column) != None):
+            if (board.piece_at(i*8+column).symbol() == "p"):
                 return False
     for i in range(row+1, 8):
-        if (board.piece_at(i*8+(column+1)) != None):
-            if (board.piece_at(i*8+(column+1)).symbol() == "p"):
-                return False
+        if (column >= 1):
+            if (board.piece_at(i*8+(column-1)) != None):
+                if (board.piece_at(i*8+(column-1)).symbol() == "p"):
+                    return False
+    for i in range(row+1, 8):
+        if (column <= 6):
+            if (board.piece_at(i*8+(column+1)) != None):
+                if (board.piece_at(i*8+(column+1)).symbol() == "p"):
+                    return False
     return True
 
 
 def count_passed_pawn(board, color):
     # returns number of passed pawns for COLOR
+    passed_pawns = 0
     for i in range(8):
         # count number of pawns in column i
-        passed_pawns = 0
         for j in range(8):
             if (board.piece_at(i*8+j) != None and color == chess.BLACK):
                 if (board.piece_at(i*8+j).symbol() == "p"):
-                    print(i, " ", j)
-                    # TODO: fix last argument.
                     if (is_passed_pawn_black(board, i, j)):
                         passed_pawns += 1
             elif (board.piece_at(i*8+j) != None and color == chess.WHITE):
                 if (board.piece_at(i*8+j).symbol() == "P"):
-                    print(i, " ", j)
                     if (is_passed_pawn_white(board, i, j)):
                         passed_pawns += 1
     return passed_pawns
@@ -57,77 +78,119 @@ def bishop_attack(board, square, color):
 def knight_attack(board, square, color):
     # returns True if SQUARE is attacked by opposing color knight
     # NOTE: this function does not check if the square is occupied by a piece
-    if (color == chess.WHITE):
-        if (board.piece_at(square+17) != None):
-            if (board.piece_at(square+17).symbol() == "n"):
-                return True
-        if (board.piece_at(square+15) != None):
-            if (board.piece_at(square+15).symbol() == "n"):
-                return True
-        if (board.piece_at(square+10) != None):
-            if (board.piece_at(square+10).symbol() == "n"):
-                return True
-        if (board.piece_at(square+6) != None):
-            if (board.piece_at(square+6).symbol() == "n"):
-                return True
-        if (board.piece_at(square-17) != None):
-            if (board.piece_at(square-17).symbol() == "n"):
-                return True
-        if (board.piece_at(square-15) != None):
-            if (board.piece_at(square-15).symbol() == "n"):
-                return True
-        if (board.piece_at(square-10) != None):
-            if (board.piece_at(square-10).symbol() == "n"):
-                return True
-        if (board.piece_at(square-6) != None):
-            if (board.piece_at(square-6).symbol() == "n"):
-                return True
-    else:
-        if (board.piece_at(square+17) != None):
-            if (board.piece_at(square+17).symbol() == "N"):
-                return True
-        if (board.piece_at(square+15) != None):
-            if (board.piece_at(square+15).symbol() == "N"):
-                return True
-        if (board.piece_at(square+10) != None):
-            if (board.piece_at(square+10).symbol() == "N"):
-                return True
-        if (board.piece_at(square+6) != None):
-            if (board.piece_at(square+6).symbol() == "N"):
-                return True
-        if (board.piece_at(square-17) != None):
-            if (board.piece_at(square-17).symbol() == "N"):
-                return True
-        if (board.piece_at(square-15) != None):
-            if (board.piece_at(square-15).symbol() == "N"):
-                return True
-        if (board.piece_at(square-10) != None):
-            if (board.piece_at(square-10).symbol() == "N"):
-                return True
-        if (board.piece_at(square-6) != None):
-            if (board.piece_at(square-6).symbol() == "N"):
-                return True
+    piece = "n" if color == chess.WHITE else "N"
+
+    if (get_column(square) <= 6 and get_row(square) <= 5 and board.piece_at(square+17) != None):
+        if (board.piece_at(square+17).symbol() == piece):
+            return True
+    if (get_column(square) >= 1 and get_row(square) <= 5 and board.piece_at(square+15) != None):
+        if (board.piece_at(square+15).symbol() == piece):
+            return True
+    if (get_column(square) <= 5 and get_row(square) <= 6 and board.piece_at(square+10) != None):
+        if (board.piece_at(square+10).symbol() == piece):
+            return True
+    if (get_column(square) >= 1 and get_row(square) <= 6 and board.piece_at(square+6) != None):
+        if (board.piece_at(square+6).symbol() == piece):
+            return True
+    if (get_column(square) >= 1 and get_row(square) >= 2 and board.piece_at(square-17) != None):
+        if (board.piece_at(square-17).symbol() == piece):
+            return True
+    if (get_column(square) <= 6 and get_row(square) >= 2 and board.piece_at(square-15) != None):
+        if (board.piece_at(square-15).symbol() == piece):
+            return True
+    if (get_column(square) >= 2 and get_row(square) >= 1 and board.piece_at(square-10) != None):
+        if (board.piece_at(square-10).symbol() == piece):
+            return True
+    if (get_column(square) <= 5 and get_row(square) >= 1 and board.piece_at(square-6) != None):
+        if (board.piece_at(square-6).symbol() == piece):
+            return True
     return False
 
 
 def pawn_attack(board, square, color):
     # returns True if SQUARE is attacked by opposing color pawn
     # NOTE: this function does not check if the square is occupied by a piece
+
     if (color == chess.WHITE):
-        if (board.piece_at(square+9) != None):
+        if (get_column(square) <= 6 and get_row(square) <= 6 and board.piece_at(square+9) != None):
             if (board.piece_at(square+9).symbol() == "p"):
                 return True
-        if (board.piece_at(square+7) != None):
+        if (get_column(square) >= 1 and get_row(square) <= 6 and board.piece_at(square+7) != None):
             if (board.piece_at(square+7).symbol() == "p"):
                 return True
     else:
-        if (board.piece_at(square-9) != None):
+        if (get_column(square) >= 1 and get_row(square) >= 1 and board.piece_at(square-9) != None):
             if (board.piece_at(square-9).symbol() == "P"):
                 return True
-        if (board.piece_at(square-7) != None):
+        if (get_column(square) <= 6 and get_row(square) >= 1 and board.piece_at(square-7) != None):
             if (board.piece_at(square-7).symbol() == "P"):
                 return True
     return False
+
+def knight_control(board, square, color):
+    piece = "N" if color == chess.WHITE else "n"
+
+    count = 0
+    if (get_column(square) <= 6 and get_row(square) <= 5 and board.piece_at(square+17) != None):
+        if (board.piece_at(square+17).symbol() == piece):
+            count += 1
+    if (get_column(square) >= 1 and get_row(square) <= 5 and board.piece_at(square+15) != None):
+        if (board.piece_at(square+15).symbol() == piece):
+            count += 1
+    if (get_column(square) <= 5 and get_row(square) <= 6 and board.piece_at(square+10) != None):
+        if (board.piece_at(square+10).symbol() == piece):
+            count += 1
+    if (get_column(square) >= 1 and get_row(square) <= 6 and board.piece_at(square+6) != None):
+        if (board.piece_at(square+6).symbol() == piece):
+            count += 1
+    if (get_column(square) >= 1 and get_row(square) >= 2 and board.piece_at(square-17) != None):
+        if (board.piece_at(square-17).symbol() == piece):
+            count += 1
+    if (get_column(square) <= 6 and get_row(square) >= 2 and board.piece_at(square-15) != None):
+        if (board.piece_at(square-15).symbol() == piece):
+            count += 1
+    if (get_column(square) >= 2 and get_row(square) >= 1 and board.piece_at(square-10) != None):
+        if (board.piece_at(square-10).symbol() == piece):
+            count += 1
+    if (get_column(square) <= 5 and get_row(square) >= 1 and board.piece_at(square-6) != None):
+        if (board.piece_at(square-6).symbol() == piece):
+            count += 1
+    return count
+
+def pawn_control(board, square, color):
+    # returns True if SQUARE is controlled by opposing color pawn
+    # NOTE: this function does not check if the square is occupied by a piece
+    count = 0
+    if (color == chess.BLACK):
+        if (get_column(square) <= 6 and get_row(square) <= 6 and board.piece_at(square+9) != None):
+            if (board.piece_at(square+9).symbol() == "p"):
+                count += 1
+        if (get_column(square) >= 1 and get_row(square) <= 6 and board.piece_at(square+7) != None):
+            if (board.piece_at(square+7).symbol() == "p"):
+                count += 1
+    else:
+        if (get_column(square) >= 1 and get_row(square) >= 1 and board.piece_at(square-9) != None):
+            if (board.piece_at(square-9).symbol() == "P"):
+                count += 1
+        if (get_column(square) <= 6 and get_row(square) >= 1 and board.piece_at(square-7) != None):
+            if (board.piece_at(square-7).symbol() == "P"):
+                count += 1
+    return count
+
+def total_control(board, color):
+    s = ""
+    for i in range(8):
+        tmp = ""
+        for j in range(8):
+            count = 0
+            count += knight_control(board, i*8+j, color)
+            count += pawn_control(board, i*8+j, color)
+            # print("Total control for ", color, " at square ", i*8+j, " is ", count)
+            tmp += str(count) + " "
+        s = tmp + "\n" + s
+    print(s)
+            
+
 
 
 def count_black_double_pawns(board):
@@ -235,13 +298,17 @@ for i in move_list:
     board.push(i)
 print(board)
 print(stockfish.get_board_visual())
-print(material_count(board))
-print(white_has_bishop_pair(board))
-print(black_has_bishop_pair(board))
-print(king_pawn_distance(board))
-print(count_white_double_pawns(board))
-print(count_black_double_pawns(board))
+print("White material count: ", material_count(board)[0])
+print("Black material count: ", material_count(board)[1])
+print("White has bishop pair?: ", white_has_bishop_pair(board))
+print("Black has bishop pair?: ", black_has_bishop_pair(board))
+print("Shortest distance from WHITE king to WHITE pawn: ", king_pawn_distance(board)[0])
+print("Shortest distance from BLACK king to BLACK pawn: ", king_pawn_distance(board)[1])
+print("Number of sets of doubled pawns for white: ", count_white_double_pawns(board))
+print("Number of sets of doubled pawns for black: ", count_black_double_pawns(board))
 print(pawn_attack(board, chess.G8, chess.BLACK))
 print(knight_attack(board, chess.G5, chess.WHITE))
-print(count_passed_pawn(board, chess.WHITE))
+print("Number of passed pawns for white: ", count_passed_pawn(board, chess.WHITE))
+print("Number of passed pawns for black: ", count_passed_pawn(board, chess.BLACK))
+print("Total control for white: ", total_control(board, chess.WHITE))
 # print(material_count(stockfish.board()))
