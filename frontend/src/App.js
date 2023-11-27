35a94b45c0dde6962e7c218084b747630829412b
nlao1 <nlao@seas.upenn.edu>
2023-11-27 02:53:48 -0500

    moved board to play page, verifying style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,47 +1,25 @@
-import './App.css';
-import Board from './components/Board/Board';
-import { reducer } from './reducer/reducer'
-import { useReducer } from 'react'
-import { initGameState } from './constants';
-import AppContext from './contexts/Context'
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import Play from './pages/Play';
-import About from './pages/About';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Control from './components/Control/Control';
-import TakeBack from './components/Control/bits/TakeBack';
-import MovesList from './components/Control/bits/MovesList';
-
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Play from "./pages/Play";
+import About from "./pages/About";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-    const [appState, dispatch] = useReducer(reducer, initGameState)
-    const providerState = {
-      appState,
-      dispatch
-    }
-    return (
-      <AppContext.Provider value={providerState}>
-        <div className="App">
-          <Router> 
-            <Navbar />
-            <Routes>
-              <Route path="/" exact component={Home} />
-              <Route path="/play" exact component={Play} />
-              <Route path="/about" exact component={About} />
-            </Routes>
-            <Board />
-            <Control>
-                <MovesList/>
-                <TakeBack/>
-            </Control>
-            <Footer />
-          </Router>
-        </div>
-      </AppContext.Provider>
-    )
-  }
-  
-  export default App;
-  
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/play" exact element={<Play />} />
+          <Route path="/about" exact component={About} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
--- frontend/src/pages/Play.js
+++ frontend/src/pages/Play.js
@@ -1,12 +1,35 @@
-import React from 'react'
-import Board from '../components/Board/Board'
+import React from "react";
+import { reducer } from "../reducer/reducer";
+import { useReducer } from "react";
+import { initGameState } from "../constants";
+import AppContext from "../contexts/Context";
+import Board from "../components/Board/Board";
+import Control from "../components/Control/Control";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import TakeBack from "../components/Control/bits/TakeBack";
+import MovesList from "../components/Control/bits/MovesList";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function Play() {
+  const [appState, dispatch] = useReducer(reducer, initGameState);
+  const providerState = {
+    appState,
+    dispatch,
+  };
   return (
-    <div>
-      <Board />
-    </div>
-  )
+    <AppContext.Provider value={providerState}>
+      <div className="App">
+        <Navbar />
+        <Board />
+        <Control>
+          <MovesList />
+          <TakeBack />
+        </Control>
+        <Footer />
+      </div>
+    </AppContext.Provider>
+  );
 }
 
-export default Play
+export default Play;
