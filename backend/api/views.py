fb5aa547ce9ac88ba8e70d59c97a511056f2e9f7
nlao1 <nlao@seas.upenn.edu>
2024-01-17 18:47:09 -0500

    new backend setup with fastapi

--- backend/api/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-
-@api_view(['GET'])
-def getData(request):
-    person = {'name': 'admin'}
-    return Response(person)
