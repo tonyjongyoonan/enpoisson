9eb23489d522b9bd5df8f7870781a88b117dd784
Ubuntu <ubuntu@ip-172-31-76-72.ec2.internal>
2024-02-28 07:26:10 +0000

    added model image, new experiment with SENettwo

--- research/notebooks/models.py
+++ research/notebooks/models.py
@@ -254,7 +254,7 @@ class ConvBlock(nn.Module):
 
 class ConvBlockTwo(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride, padding):
-        super(ConvBlock, self).__init__()
+        super(ConvBlockTwo, self).__init__()
         self.conv1 = nn.Conv2d(
             in_channels, out_channels, kernel_size, stride, padding, bias=False
         )
