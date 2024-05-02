5f67f989b5725d9e7d0bed9c621815507c8a2bb8
ishlal <ilal@seas.upenn.edu>
2024-05-02 15:40:59 -0400

    diff

--- backend/main.py
+++ backend/main.py
@@ -10,6 +10,8 @@ from stockfish import Stockfish
 import numpy as np
 import platform
 
+from heuristics import check_hanging
+
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
@@ -140,6 +142,13 @@ def get_difficulty(position: Difficulty):
 
     moves_uci = [move["Move"] for move in stockfish_top_5_not_none]
     evals = [int(move["Centipawn"]) / 100.0 for move in stockfish_top_5_not_none]
+    # sort evals in descending order
+    evals = sorted(evals, reverse=True)
+
+    if check_hanging(chess_board, turn):
+        evals[0] = (evals[1] + evals[2] + evals[3] + evals[4]) / 4
+
+
     moves_san = [chess_board.san(chess.Move.from_uci(move)) for move in moves_uci]
     engine = chess_engines[(position.elo, position.is_white_move)]
     probabilities = [
